a86a6ed4d29fc54429188efbabbf031815421dba
Brk <kazancibulent@gmail.com>
2017-10-17 00:21:26 +0300

    added example containers for text content page and image annotation page.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '@store';
-import TestContainer from '@containers/TestContainer';
+import TextContentExampleContainer from '@containers/TextContentExampleContainer';
+import ImageContentExampleContainer from '@containers/ImageContentExampleContainer';
 
 const store = configureStore();
 
 const Kernel = () => (
   <Provider store={store}>
-    <TestContainer/>
+    <ImageContentExampleContainer/>
   </Provider>
 )
 
