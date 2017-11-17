77dea5f10f53b0a879423821420f4c0e4527f66e
Brk <kazancibulent@gmail.com>
2017-11-18 00:08:32 +0300

    implemented propert fetch of semantic bodies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,9 @@
-//export const API_URI = 'http://localhost:8080/api/v1';
+export const API_URI = 'http://localhost:8080/api/v1';
 export const AUTH_TOKEN_KEY = 'AUTH_TOKEN_KEY';
 export const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN_KEY';
+<<<<<<< HEAD
 export const API_URI = 'http://living-history.gkc.host/api/v1';
 export const USERNAME = 'USERNAME'
+=======
+//export const API_URI = 'http://living-history.gkc.host/api/v1';
+>>>>>>> 1782169... implemented propert fetch of semantic bodies
