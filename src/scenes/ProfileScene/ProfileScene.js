b1751f6cc17eeb9becb41969542ecd759c971168
kahyabatuhan <kahyabatuhan@gmail.com>
2017-11-30 14:18:39 +0300

    refactor: removed unused imports and syntax alignment

--- src/scenes/ProfileScene/ProfileScene.js
+++ src/scenes/ProfileScene/ProfileScene.js
@@ -1,68 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import style from '@style/main';
 import privateStyle from './style';
-import { ZImageView, ZTextArea, ZButton } from '@components/index';
 import ReactNative from 'react-native';
 import { fetchContents } from '@actions';
-import { AnnotationFactory } from '@common';
-import { AnnotationTypes } from '@enums';
-import { ImageTarget, BaseAnnotationBody } from '@models';
 const {
   ScrollView,
   View,
-  TextInput,
   Image,
   Text,
   TouchableHighlight,
-  StyleSheet,
-} = ReactNative
-
-
+} = ReactNative;
 
 export class ProfileScene extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
       this.props.fetchContents();
     }
 
-    render(){
+    render() {
       
-      let contents = this.props.appData.contents;
-
+      const contents = this.props.appData.contents;
+      
       return(
       <View style={privateStyle.scene}>
 
         <ScrollView style={privateStyle.scrollSection}>
           {contents.map((content) => {
             return (
-              <TouchableHighlight key={content.id} 
+              <TouchableHighlight 
+                  key={content.id} 
                   style={privateStyle.resultButton} 
-                  onPress={ () => this.props.navigation.navigate('ContentDetail', { contentId: content.id }) }>
+                  onPress={() => this.props.navigation.navigate('ContentDetail', { contentId: content.id }) }>
                 <View>
-                  <Image source={ { uri: content.cover_image } } style={privateStyle.resultImage} />
-                  <Text style={ privateStyle.resultText } >{content.title}</Text>
+                  <Image source={{ uri: content.cover_image }} style={privateStyle.resultImage} />
+                  <Text style={privateStyle.resultText} >{content.title}</Text>
                 </View>
               </TouchableHighlight>
-            )
+            );
           })}
         </ScrollView>
       </View>
-      )
+      );
     }
-
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     appData: state.HomeReducer
-  }
+  };
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
     fetchContents: () => dispatch(fetchContents())
-  }
+  };
 }
 
-export default connect( mapStateToProps, mapDispatchToProps)(ProfileScene);
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileScene);
