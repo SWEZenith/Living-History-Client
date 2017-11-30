2d490e2084ac3a339fb2fa4166514658f27fbcac
kahyabatuhan <kahyabatuhan@gmail.com>
2017-11-30 18:38:51 +0300

    ProfileScene: placeholder for annotations

--- src/scenes/ProfileScene/ProfileScene.js
+++ src/scenes/ProfileScene/ProfileScene.js
@@ -20,18 +20,24 @@ export class ProfileScene extends Component {
     render() {
       
       const contents = this.props.appData.userContents;
+      const { headerStyle, containerStyle, itemStyle } = styles;
       
       return(
       <View style={privateStyle.scene}>
 
+      <View style={headerStyle}>
+        <Text> User Profile </Text>
+      </View>
+
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', padding: 5 }} >
         <ScrollView style={privateStyle.scrollSection}>
           {contents.map((content) => {
             return (
               <TouchableHighlight 
                   key={content.id} 
-                  style={privateStyle.resultButton} 
+                  style={privateStyle.resultButton,{paddingLeft: 15}} 
                   onPress={() => this.props.navigation.navigate('ContentDetail', { contentId: content.id }) }>
-                <View>
+                <ScrollView>
                   <Image source={{ uri: content.cover_image }} style={privateStyle.resultImage} />
                   <Text style={privateStyle.resultText} >{content.title}</Text>
                 </View>
@@ -39,6 +45,23 @@ export class ProfileScene extends Component {
             );
           })}
         </ScrollView>
+
+        <ScrollView style={privateStyle.scrollSection}>
+          {contents.map((content) => {
+            return (
+              <TouchableHighlight 
+                  key={content.id} 
+                  style={privateStyle.resultButton,{paddingLeft: 15}} 
+                  onPress={() => this.props.navigation.navigate('ContentDetail', { contentId: content.id }) }>
+                <View>
+                  <Text style={privateStyle.resultText} >{content.title}</Text>
+                </View>
+              </TouchableHighlight>
+            );
+          })}
+        </ScrollView>
+      </View>
+
       </View>
       );
     }
@@ -55,5 +78,33 @@ function mapDispatchToProps(dispatch) {
     fetchUserContents: () => dispatch(fetchUserContents())
   };
 }
+
+const styles = {
+  headerStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+    fontSize: 20
+  },
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
+  },
+  itemStyle: {
+    paddingLeft: 15
+  }
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileScene);
