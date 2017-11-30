eac217cde346a2d4f0999b2ee5d7ef094ef9698a
kahyabatuhan <kahyabatuhan@gmail.com>
2017-11-30 14:02:44 +0300

    ProfileScene derived from HomeScene

--- src/scenes/ProfileScene/ProfileScene.js
+++ src/scenes/ProfileScene/ProfileScene.js
@@ -1,34 +1,67 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { style } from '@style/main';
+import style from '@style/main';
 import privateStyle from './style';
-import { View, Text } from 'react-native';
+import { ZImageView, ZTextArea, ZButton } from '@components/index';
+import ReactNative from 'react-native';
+import { fetchContents } from '@actions';
+import { AnnotationFactory } from '@common';
+import { AnnotationTypes } from '@enums';
+import { ImageTarget, BaseAnnotationBody } from '@models';
+const {
+  ScrollView,
+  View,
+  TextInput,
+  Image,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+} = ReactNative
 
 
 
 export class ProfileScene extends Component {
 
-  render(){
-  	return(
-      <View style={[style.zPage]}>
-        <View style={{flex:2}}>
-          <Text>
-            ProfileScene Scene
-          </Text>
-        </View>
+    componentDidMount(){
+      this.props.fetchContents();
+    }
+
+    render(){
+      
+      let contents = this.props.appData.contents;
+
+      return(
+      <View style={privateStyle.scene}>
+
+        <ScrollView style={privateStyle.scrollSection}>
+          {contents.map((content) => {
+            return (
+              <TouchableHighlight key={content.id} 
+                  style={privateStyle.resultButton} 
+                  onPress={ () => this.props.navigation.navigate('ContentDetail', { contentId: content.id }) }>
+                <View>
+                  <Image source={ { uri: content.cover_image } } style={privateStyle.resultImage} />
+                  <Text style={ privateStyle.resultText } >{content.title}</Text>
+                </View>
+              </TouchableHighlight>
+            )
+          })}
+        </ScrollView>
       </View>
-	  )
-  }
+      )
+    }
 
 }
 
 function mapStateToProps (state) {
   return {
+    appData: state.HomeReducer
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return {
+    fetchContents: () => dispatch(fetchContents())
   }
 }
 
--- src/scenes/ProfileScene/style.js
+++ src/scenes/ProfileScene/style.js
@@ -3,7 +3,54 @@ import { StyleSheet } from 'react-native';
 
 
 const main = StyleSheet.create({
-
+  scene: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  searchSection: {
+    height: 40,
+    flexDirection: 'row',
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+    padding: 5,
+  },
+  scrollSection: {
+    flex: 0.8
+  },
+  searchButton: {
+    flex: 0.12,
+    height: 30,
+    backgroundColor:'#9B51E0',
+    padding:5,
+    borderRadius:50,
+  },
+  createButton: {
+    flex: 1,
+    height: 30,
+    backgroundColor:'#9B51E0',
+    padding:5,
+    borderRadius:50,
+  },
+  searchText: {
+    color:'#fff',
+    textAlign:'center',
+  },
+  resultButton: {
+    flex: 0.3,
+  },
+  searchInput: {
+    flex: 0.88,
+    height: 40,
+    backgroundColor: 'white'
+  },
+  resultText: {
+    backgroundColor: '#000',
+    color: '#FFF',
+    height: 20,
+  },
+  resultImage: {
+    height: 150,
+  }
 });
 
 export default main;
