043eb76c473c360905370aa13e16078f2883d437
kahyabatuhan <kahyabatuhan@gmail.com>
2017-12-11 23:06:42 +0300

    added navigations for profile

--- src/navigation/ProfileStackNavigator.js
+++ src/navigation/ProfileStackNavigator.js
@@ -1,11 +1,15 @@
 import { StackNavigator } from 'react-navigation';
 
-import { ProfileScene } from '@scenes';
+import { 
+	ProfileScene,
+	ContentDetailScene,
+	AnnotationDetailScene,	
+} from '@scenes';
 
 const ProfileStackNavigator = StackNavigator({
-  Profile: {
-    screen: ProfileScene
-  }
+  Profile: { screen: ProfileScene },
+  ContentDetail: { screen: ContentDetailScene },
+  AnnotationDetail: { screen: AnnotationDetailScene },
 });
 
 export default ProfileStackNavigator;
